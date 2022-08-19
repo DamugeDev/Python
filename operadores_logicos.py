d2ba76a488680499789cd997ff905df87ee38a7e
Damuge Leonardo <89780160+DamugeDev@users.noreply.github.com>
2022-08-19 14:50:09 +0200

    logicos

---
+++ operadores_logicos.py
@@ -0,0 +1,8 @@
+# and - resultado verdadeiro quando AMBAS as proposicoes forem VERDADEIRAS
+a=(1>8)and(3<3)
+
+# or - resultado verdadeiro quando PELO MENOS UMA das proposicoes for VERDADEIRAS
+b=(4==8/2)or('Alto'=='alto')
+
+# not - NEGACAO
+print(not b)
