54962ecba9571ef88bd0059ffccdba47fdb56a6d
Damuge Leonardo <89780160+DamugeDev@users.noreply.github.com>
2022-08-26 22:50:19 +0200

    senhas

---
+++ gerar_senha.py
@@ -0,0 +1,14 @@
+#Gerar senhas usando a biblioteca random
+import random
+
+minusculas = "abcdefghijklmnopqrstuvwxyz"
+maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numeros = "0123456789"
+simbolos = "/*!@#$%&"
+
+ciclo = minusculas+maiusculas+numeros+simbolos
+tamanho=8
+
+senha= "".join(random.sample(ciclo,tamanho))
+
+print(senha)
